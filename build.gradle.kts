89340095ae208f9f3e98090c189608ad77a158c6
Jeff Fang <jeff.fang@airwallex.com>
2022-03-12 22:46:36 +0800

    update to version 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    id("com.airwallex.grpc-spring") version "1.2.0" apply false
+    id("com.airwallex.grpc-spring") version "1.2.1" apply false
 }
 
 group = "com.airwallex"
