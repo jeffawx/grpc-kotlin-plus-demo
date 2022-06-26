324add35de64caa480a552a70feb43bb93ff4d24
Jeff Fang <jeff.fang@airwallex.com>
2022-06-26 18:01:23 +0800

    bump version to 1.2.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    id("com.airwallex.grpc-spring") version "1.2.7" apply false
+    id("com.airwallex.grpc-spring") version "1.2.8" apply false
 }
 
 group = "com.airwallex"
