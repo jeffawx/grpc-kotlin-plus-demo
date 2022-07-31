6b4438674ea8ccd1649c40d636acf09783c02f42
Jeff Fang <jeff.fang@airwallex.com>
2022-07-31 14:43:53 +0800

    update client

--- greeting-web/src/main/kotlin/com/airwallex/grpc/demo/GreeterClient.kt
+++ greeting-web/src/main/kotlin/com/airwallex/grpc/demo/GreeterClient.kt
@@ -1,13 +1,20 @@
 package com.airwallex.grpc.demo
 
 import com.airwallex.grpc.annotations.GrpcClient
+import com.airwallex.grpc.annotations.GrpcMethod
 import com.airwallex.grpc.demo.Greeting3.HelloReply3
 import com.airwallex.grpc.demo.Greeting3.HelloRequest3
 import com.airwallex.mapstruct.protobuf.ProtobufConfig
 import org.mapstruct.Mapper
 
-@GrpcClient("greeter", name = "Greeter3", mapperInterface = GreeterMapper::class)
+@GrpcClient("greeter", name = "Greeter3")
 interface GreeterClient {
+    @GrpcMethod(
+        requestMapper = GreetRequestMapper::class,
+        requestMapperProtoType = HelloRequest3::class,
+        responseMapper = GreetReplyMapper::class,
+        responseMapperProtoType = HelloReply3::class
+    )
     suspend fun sayHello(request: GreetRequest): GreetReply
 }
 
@@ -17,12 +24,9 @@ class GreetReply(val message: String)
 
 /**
  * Explicit mapping layer with mapstruct.
- *
- * This example uses default method names, can use custom method name with @GrpcMethod in service method.
  */
 @Mapper(config = ProtobufConfig::class)
-interface GreeterMapper {
-    // for client
-    fun sayHelloMapRequestToProto(request: GreetRequest): HelloRequest3
-    fun sayHelloMapResponseFromProto(response: HelloReply3): GreetReply
-}
+interface GreetRequestMapper : com.airwallex.mapstruct.protobuf.MessageMapper<HelloRequest3, GreetRequest>
+
+@Mapper(config = ProtobufConfig::class)
+interface GreetReplyMapper : com.airwallex.mapstruct.protobuf.MessageMapper<HelloReply3, GreetReply>
