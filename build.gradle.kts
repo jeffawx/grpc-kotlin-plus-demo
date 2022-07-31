b631a6287ab4714cec078275207d0a7b602d1893
Jeff Fang <jeff.fang@airwallex.com>
2022-07-31 14:21:31 +0800

    upgrade to release 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    id("com.airwallex.grpc-spring") version "1.2.8" apply false
+    id("org.springframework.boot") version "2.7.2" apply false
+    kotlin("jvm") version "1.6.21" apply false
+    kotlin("plugin.spring") version "1.6.21" apply false
+    id("com.airwallex.grpc") version "1.3.0" apply false
 }
 
 group = "com.airwallex"
@@ -17,7 +20,11 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "com.airwallex.grpc-spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "com.airwallex.grpc")
 
     java.sourceCompatibility = JavaVersion.VERSION_11
 
@@ -32,5 +39,14 @@ subprojects {
         test {
             useJUnitPlatform()
         }
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        implementation("com.airwallex.grpc:grpc-spring-boot-starter")
     }
 }
